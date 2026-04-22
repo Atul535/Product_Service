c091d214549e3fcb3dc8b8d73437a1326d532bff
Atul Mishra <atulmish99@gmail.com>
2026-04-22 18:55:03 +0530

    product service modified

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -2,51 +2,51 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const prisma = require('../utils/prisma');
 
-const register = async(req,res,next)=>{
-    try{
-        const {email,password}=req.body;
+const register = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
 
         //check if user already exists
-        const existingUser=await prisma.user.findUnique({where:{email}});
-        if(existingUser){
-            return res.status(400).json({message:'User already exists!'})   
+        const existingUser = await prisma.user.findUnique({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists!' })
         }
 
         // hash password
-        const hashedPassword=await bcrypt.hash(password,10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         //create user 
-        const user= await prisma.user.create({
-            data:{email,password:hashedPassword}
+        const user = await prisma.user.create({
+            data: { email, password: hashedPassword }
         });
-        res.status(201).json({message:'User registered successfully',userID:user.id});
-    }catch(error){
+        res.status(201).json({ message: 'User registered successfully', userID: user.id });
+    } catch (error) {
         next(error);
     }
 };
 
-const login= async(req,res,next)=>{
-    try{
-        const {email, password}=req.body;
+const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
 
         // find user
-        const user =await prisma.user.findUnique({where:{email}});
+        const user = await prisma.user.findUnique({ where: { email } });
 
-        if(!user){
-            return res.status(400).json({message:'Invalid credentials!'});
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials!' });
         }
 
         //check password
-        const isMatch=await bcrypt.compare(password,user.password);
-        if(!isMatch){
-            return res.status(400).json({message:'Invalid credentials!'});
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials!' });
         }
         //generate JWT token
-        const token=jwt.sign({userId:user.id},process.env.JWT_SECRET,{expiresIn:'1h'});
-        res.json({message:'Login successful',token});
-    }catch(error){
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        res.json({ message: 'Login successful', token });
+    } catch (error) {
         next(error);
     }
 };
 
-module.exports={register,login};
+module.exports = { register, login };
