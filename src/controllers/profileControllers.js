0684c584a7e7e4c311e6ce46fefb3a8ede5db30c
Atul Mishra <atulmish99@gmail.com>
2026-04-29 12:53:53 +0530

    updateprofile

--- src/controllers/profileControllers.js
+++ src/controllers/profileControllers.js
@@ -22,4 +22,32 @@ const getAllUsers = async (req, res, next) => {
     }
 }
 
-module.exports = { getProfile, getAllUsers };
+const updateProfile = async (req, res, next) => {
+    try {
+        const { name, oldPassword, newPassword } = req.body;
+        const userId = req.user.userId;
+        const user = await prisma.user.findUnique({ where: { id: userId } });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found!!!' });
+        }
+
+        const updateData = {};
+        if (name) {
+            updateData.name = name;
+        }
+        if (oldPassword && newPassword) {
+            const isMatch = await bcrypt.compare(oldPassword, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Incorrect old password!' });
+            }
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            updateData.password = hashedPassword;
+        }
+        const updatedUser = await prisma.user.update({ where: { id: userId }, data: updateData, select: { id: true, name: true, email: true, } });
+        res.json({ message: 'Profile updated successfully!!' })
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { getProfile, getAllUsers, updateProfile };
--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getProfile, getAllUsers } = require('../controllers/profileControllers');
+const { getProfile, getAllUsers, updateProfile } = require('../controllers/profileControllers');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get('/getUser', authMiddleware, getProfile);
 router.get('/getAllUsers', authMiddleware, getAllUsers);
+router.put('/updateProfile', authMiddleware, updateProfile);
 
 module.exports = router;
