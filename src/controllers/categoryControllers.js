5bf7d044426b3bc2d9f21d529fda63bb306fe913
Atul Mishra <atulmish99@gmail.com>
2026-05-01 15:02:00 +0530

    image in category

--- src/controllers/categoryControllers.js
+++ src/controllers/categoryControllers.js
@@ -5,11 +5,12 @@ const prisma = new PrismaClient();
 const createCategory = async (req, res, next) => {
     try {
         const { name, description } = req.body;
+        const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
         if (!name) {
             return res.status(400).json({ message: 'Category name is required' });
         }
         const category = await prisma.category.create({
-            data: { name, description },
+            data: { name, description, imageUrl },
         });
         res.status(201).json({ message: 'Category created successfully', data: category });
     } catch (error) {
@@ -34,9 +35,16 @@ const updateCategory = async (req, res, next) => {
     try {
         const { id } = req.params;
         const { name, description } = req.body;
+        const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
+
+        const dataToUpdate = {
+            ...(name && { name }),
+            ...(description && { description }),
+            ...(imageUrl && { imageUrl }),
+        };
         const category = await prisma.category.update({
             where: { id: parseInt(id) },
-            data: { name, description }
+            data: dataToUpdate
         });
         res.status(200).json({ message: 'Category Updated', data: category });
     } catch (error) {
--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createCategory, getAllCategories, updateCategory, deleteCategory } = require('../controllers/categoryControllers');
 const authMiddleware = require('../middleware/authMiddleware');
+const uploadMiddleware = require('../middleware/uploadMiddleware');
 const router = express.Router();
 
-router.post('/createCategory', authMiddleware, createCategory);
+router.post('/createCategory', authMiddleware, upload.single('image'), createCategory);
 router.get('/getAllCategories', authMiddleware, getAllCategories);
-router.put('/updateCategory/:id', authMiddleware, updateCategory);
+router.put('/updateCategory/:id', authMiddleware, upload.single('image'), updateCategory);
 router.delete('/deleteCategory/:id', authMiddleware, deleteCategory);
 
 module.exports = router;
